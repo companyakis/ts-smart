f01c396d80510681e946d8dc3fcb37b35c7fd6a0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 01:01:48 +0300

    Create part2-0005-arrays2.ts

---
+++ part2-0005-arrays2.ts
@@ -0,0 +1,15 @@
+let employeeInfo = {
+    name: "Ayhan",
+    id: 1021,
+    areas: ["FinTech", "Finance", "Audit"]
+}
+
+for (const area of employeeInfo.areas) {
+  
+    console.log(`Area: ${area}`)
+}
+
+
+// Area: FinTech
+// Area: Finance
+// Area: Audit
