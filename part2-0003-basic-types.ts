8ac0a6fa6a9642c4db49f2681d44e576c1b90582
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-27 22:59:31 +0300

    Create part2-0003-basic-types.ts

---
+++ part2-0003-basic-types.ts
@@ -0,0 +1,23 @@
+// number
+
+const pi: number = 22 / 7
+
+let year: number = 2024
+
+let diff = 100 - year
+
+console.log(pi, year, diff) // 3.142857142857143 2024 -1924
+
+// string
+
+const myName: string = "Mustafa Büyükdereli"
+
+let firstLetter = "a"
+
+let country = 'Turkiye'
+
+// bool
+
+let isCompleted: boolean = false
+
+let willBeReady: boolean = true
