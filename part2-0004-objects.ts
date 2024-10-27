71cc2172e139631bb5bfd6bea402e4b286c587fe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 00:16:51 +0300

    Create part2-0004-objects.ts

---
+++ part2-0004-objects.ts
@@ -0,0 +1,17 @@
+const employeeAygul = {
+
+    name : "Aygül",
+    department: "Operations",
+    id: 101
+
+}
+
+console.log("Name: ", employeeAygul.name)
+
+console.log("Department: ", employeeAygul.department)
+
+console.log("ID: ", employeeAygul.id)
+
+// Name:  Aygül
+// Department:  Operations
+// ID:  101
