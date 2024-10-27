257b46c2e1ae42dbbeba2d5275ce8cefafb10680
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 00:57:38 +0300

    Create part2-0005-arrays1.ts

---
+++ part2-0005-arrays1.ts
@@ -0,0 +1,7 @@
+let cities: string[] = ["Ankara", "İstanbul", "İzmir", "Adana"]
+
+let years: number[] = [2018, 2020, 2024]
+
+let differentData: any[] = ["Mustafa", 2024, true] // not good!
+
+let completed: boolean[] = [true, false, true, true, false]
