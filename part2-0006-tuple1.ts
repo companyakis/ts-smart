4ab1bba1c638ac18788012d3109cea435aff010a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 08:55:26 +0300

    Create part2-0006-tuple1.ts

---
+++ part2-0006-tuple1.ts
@@ -0,0 +1,16 @@
+let emp_hakan: [string, string, number, number]
+
+emp_hakan = ["Finance", "Expert", 104, 6_500]
+
+console.log("Employee Hakan => Department: ", emp_hakan[0])
+
+console.log("Employee Hakan => Title: ", emp_hakan[1])
+
+console.log("Employee Hakan => ID: ", emp_hakan[2])
+
+console.log("Employee Hakan => Salary $: ", emp_hakan[3])
+
+// Employee Hakan => Department:  Finance
+// Employee Hakan => Title:  Expert
+// Employee Hakan => ID:  104
+// Employee Hakan => Salary $:  6500
