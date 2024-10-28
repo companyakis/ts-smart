906cf66c5fef4439941adf5609826f7f6b45dfdb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 09:08:48 +0300

    Create part2-0007-tuple-optional.ts

---
+++ part2-0007-tuple-optional.ts
@@ -0,0 +1,9 @@
+let customer: [string, string, number?, number?, boolean?] // name, phone, salary, job, marriage
+
+customer = ["Ayhan", "05053216543"]
+
+console.log(customer) // [ 'Ayhan', '05053216543' ]
+
+customer = ["Ayhan", "05053216543", 4300]
+
+console.log(customer) // [ 'Ayhan', '05053216543', 4300]
