3fb4210569cab953d73c573afb7068701d5b6dc3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 20:12:56 +0300

    Create part2-0012-literals2.ts

---
+++ part2-0012-literals2.ts
@@ -0,0 +1,13 @@
+type Years = 1990 | 2018 | 2020 | 2024
+
+let pointYear: Years
+
+pointYear = 1990
+
+pointYear = 2018
+
+pointYear = 2024
+
+pointYear = 2020
+
+// pointYear = 1888 => Error: Type '1888' is not assignable to type 'Years'.
