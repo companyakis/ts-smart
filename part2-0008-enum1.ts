72a849ab42b54e20a4cce6302914c3430c25dc45
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 09:14:30 +0300

    Create part2-0008-enum1.ts

---
+++ part2-0008-enum1.ts
@@ -0,0 +1,13 @@
+enum Departments {
+    Fintech,
+    Finance,
+    Sales,
+    Marketing,
+    Operations,
+    Audit,
+    HR
+}
+
+let fintech_dep: Departments = Departments.Fintech
+
+console.log(fintech_dep) // 0
