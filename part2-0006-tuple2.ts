99ec0ac670d15873c2cdfee033086da29ed1f62e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 08:59:05 +0300

    Create part2-0006-tuple2.ts

---
+++ part2-0006-tuple2.ts
@@ -0,0 +1,8 @@
+function getEmployeeHakanInfo(): [string, string, number, number] {
+
+    return ["Finance", "Expert", 104, 6500]
+}
+
+let empHakan = getEmployeeHakanInfo()
+
+console.log(empHakan) // [ 'Finance', 'Expert', 104, 6500]
