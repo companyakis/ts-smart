047023c32b68fec1d33709dfa57fb26be48bc673
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 09:16:51 +0300

    Create part2-0008-enum2.ts

---
+++ part2-0008-enum2.ts
@@ -0,0 +1,14 @@
+enum Months {
+  
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7
+}
+
+let march: Months = Months.March
+
+console.log(march) // 3
