e13df39e54b1ba292b8154df8598945ef2d54f39
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 09:18:35 +0300

    Create part2-0008-enum3.ts

---
+++ part2-0008-enum3.ts
@@ -0,0 +1,14 @@
+enum Months {
+  
+    January = "January",
+    February = "February",
+    March = "March",
+    April = "April",
+    May = "May",
+    June = "June",
+    July = "July"
+}
+
+let march: Months = Months.March
+
+console.log(march) // March
