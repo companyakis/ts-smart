3da78c197bda3b92bf7a0caf31f1e229e3fa078d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 20:09:47 +0300

    Create part2-0012-literals1.ts

---
+++ part2-0012-literals1.ts
@@ -0,0 +1,11 @@
+type Areas = "AI" | "Web3" | "Algo Trade"
+
+let companyInterest: Areas
+
+companyInterest = "AI"
+
+companyInterest = "Algo Trade"
+
+companyInterest = "Web3"
+
+//companyInterest = "Accounting " => Error: Type '"Accounting "' is not assignable to type 'Areas'
