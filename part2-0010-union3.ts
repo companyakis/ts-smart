28380edb4db3d1f37557f2a9cb6465ccebb264ad
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-28 12:55:28 +0300

    Create part2-0010-union3.ts

---
+++ part2-0010-union3.ts
@@ -0,0 +1,16 @@
+let data: (string | number)[]
+
+data = [1, 2, -5]
+
+console.log(data)
+
+data = ["Mustafa", "Aygün", "Bengü", "Kültigin"]
+
+console.log(data)
+
+data = ["Mustafa", 2024, "Turkic"]
+
+console.log(data)
+
+//data = [3, false] // Error => Type 'boolean' is not assignable to type 'string | number'.
+
