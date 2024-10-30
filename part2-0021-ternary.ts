6601411d76e697f532ae436bab2dca0914b8201f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-30 22:13:04 +0300

    Create part2-0021-ternary.ts

---
+++ part2-0021-ternary.ts
@@ -0,0 +1,9 @@
+let sales: number = 2100400
+
+sales = 3000000
+
+let result: string = sales > 2500000 ? "Very good! Awesome..." : "No... I'm sorry..."
+
+// console.log(result) // No... I'm sorry...
+
+console.log(result) // Very good! Awesome...
