4bdce83280cd99a6d225c37242b1c82b86beb0f8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-30 08:54:00 +0300

    Create part2-0015-arrow-functions2.ts

---
+++ part2-0015-arrow-functions2.ts
@@ -0,0 +1,7 @@
+const perimeter = (x: number, y: number) : number => 2 * (x + y)
+
+let width = 5
+
+let height = 12
+
+console.log("Rectangular perimeter: " + perimeter(width, height)) // Rectangular perimeter: 34
