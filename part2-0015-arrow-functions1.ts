3d1f2e0c3886054febd194daff2f6bb1f2e25134
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-30 08:51:19 +0300

    Create part2-0015-arrow-functions1.ts

---
+++ part2-0015-arrow-functions1.ts
@@ -0,0 +1,12 @@
+const mutltiplication = (a: number, b: number, c: number) : number => {
+
+    return a * b * c
+}
+
+let x = 4
+
+let y = 11
+
+let z = 3.4
+
+console.log(`${x} * ${y} * ${z} = ${mutltiplication(x, y, z)}`) // 4 * 11 * 3.4 = 149.6
