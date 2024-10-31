8fc1dddee9e28d0d19ac96ba359d1e64cbefb845
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-31 23:27:11 +0300

    Create part2-0024-while2.ts

---
+++ part2-0024-while2.ts
@@ -0,0 +1,31 @@
+let counter: number = 18
+
+while (counter > 0) {
+
+    counter --
+
+    if (counter === 13) {
+
+        continue
+    }
+
+    console.log("Counter: " + counter)
+}
+
+// Counter: 17
+// Counter: 16
+// Counter: 15
+// Counter: 14
+// Counter: 12
+// Counter: 11
+// Counter: 10
+// Counter: 9
+// Counter: 8
+// Counter: 7
+// Counter: 6
+// Counter: 5
+// Counter: 4
+// Counter: 3
+// Counter: 2
+// Counter: 1
+// Counter: 0
