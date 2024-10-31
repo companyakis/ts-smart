08b66c7812c64756708860dd18cb57d82addc714
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-31 19:36:45 +0300

    Create part2-0023-for1.ts

---
+++ part2-0023-for1.ts
@@ -0,0 +1,11 @@
+let years: number[] = [1990, 2018, 2020, 2024]
+
+for (let i = 0; i < years.length; i++) {
+
+    console.log(`Year_${i + 1} => ${years[i]}`)
+}
+
+// Year_1 => 1990
+// Year_2 => 2018
+// Year_3 => 2020
+// Year_4 => 2024
