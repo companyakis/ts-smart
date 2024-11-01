f48f468267bc59feca30f5acdc285258849cd5ed
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-01 13:41:49 +0300

    Create part2-0025-foreach2.ts

---
+++ part2-0025-foreach2.ts
@@ -0,0 +1,9 @@
+const years = [1990, 2018, 2020, 2024]
+
+years.forEach((year, index) => console.log(`Year${index + 1}: ${year}`))
+
+// Year1: 1990
+// Year2: 2018
+// Year3: 2020
+// Year4: 2024
+
