efd54b651ca0dfcd0c3b9ca9cc296c12b8f6c3e0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-03 22:08:09 +0300

    Create part2-0026-interface1.ts

---
+++ part2-0026-interface1.ts
@@ -0,0 +1,17 @@
+interface Employee {
+    
+    name: string
+    department: string
+    title: string
+    salary: number
+    isMarried: boolean
+}
+
+let employee1: Employee = {
+
+    name: "Ayhan",
+    department: "HR",
+    title: "Assistant Manager",
+    salary: 75000,
+    isMarried: false
+}
